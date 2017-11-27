dc879fa69d7c205bcc96ae7d1853d91cd1e3dc6d
conangit <594946891@qq.com>
2017-11-27 22:35:20 +0800

    修正代码格式

--- usb/usb.c
+++ usb/usb.c
@@ -4,30 +4,30 @@
 
 int main(int argc, char *argv[])
 {
-	int opt;
-	char *usbno = "0";
-	char cmd[128] = {0};	
-	char *ptr = "/dev/ttyUSB";
+    int opt;
+    char *usbno = "0";
+    char cmd[128] = {0};
+    char *ptr = "/dev/ttyUSB";
 
-	while ( (opt = getopt(argc, argv, "c:")) != -1 )
+    while ( (opt = getopt(argc, argv, "c:")) != -1 )
     {
-		switch (opt)
-		{
-			case 'c':
-				usbno = optarg;		
-				break;
+        switch (opt)
+        {
+            case 'c':
+                usbno = optarg;
+                break;
 
-			default:
+            default:
                 printf("unknown option: -%c\n", (char)optopt);
                 return -1;
-		}
-	}
+        }
+    }
 
-	sprintf(cmd, "%s%s%s", "sudo picocom -b 115200 ", ptr, usbno);
-	
-	printf("%s\n", cmd);
-	system(cmd);	
+    sprintf(cmd, "%s%s%s", "sudo picocom -b 115200 ", ptr, usbno);
+    
+    printf("%s\n", cmd);
+    system(cmd);    
 
-	return 0;
+    return 0;
 }
 
